4e3892bdf14cd5ed3a58dd8c744ee9f6f6aeb98e
Ciao <ciao0958@gmail.com>
2018-01-21 17:08:26 +0800

    init clear vue webpack project

---
+++ dev/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">App.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  computed: {},
+  components: {}
+}
+</script>
+
+<style lang="sass" type="text/sass"></style>
---
+++ dev/src/router.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+let routes = []
+
+export default new Router({
+  mode: 'history',
+  routes: routes,
+})
